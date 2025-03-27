b23e73ec9989d065aaac38c20c269c6f759ffda4
namelessfifi <estefaniasantosmena@gmail.com>
2025-03-27 14:07:55 +0000

    emcabezado.c

---
+++ encabezado.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void encabezado(void)
+
+int main(int argc, char const *argv[]) {
+  
+    
+    return 0;
+}
+
+void encabezado(void)
+{
+     {
+     printf("***********************************************"\n)
+     printf("*                                             *"\n);   
+     printf("*    Universidad Pontificia Bolivariana       *"\n);
+     printf("*            Escuela Ingeniería               *"\n); 
+     printf("*           Ingeniería Eléctrica              *"\n);
+     printf("*             Estefanía Santos                *"\n);
+     printf("*                                             *"\n);
+     printf("***********************************************"\n); 
+     }
+}
